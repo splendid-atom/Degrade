d9a4d91bb4b0ab56b854e772927cc8f10b1f6d3a
xsk23 <xsk23@mails.tsinghua.edu.cn>
2025-01-28 01:38:18 +0800

    新增小地图功能

--- Degrade_project/Assets/Scripts/PlayerController.cs
+++ Degrade_project/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
-
-
     Rigidbody2D rigidbody2d;
     public float speed = 3.0f;
     public InputAction MoveActionWASD;
     Vector2 move;
     Animator animator;
-    Vector2 moveDirection = new Vector2(1,0);
-    // private Vector3 offset;
+    Vector2 moveDirection = new Vector2(1, 0);
+    public Transform arrowIndicator;  // 引用ArrowIndicator
 
     // Start is called before the first frame update
     void Start()
     {
-        // offset = Camera.main.transform.position - transform.position;
         rigidbody2d = GetComponent<Rigidbody2D>();
         MoveActionWASD.Enable();
         animator = GetComponent<Animator>();
@@ -27,22 +25,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         move = MoveActionWASD.ReadValue<Vector2>();
-        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
             moveDirection.Set(move.x, move.y);
             //sets its length to 1 but keeps its direction the same. 
             moveDirection.Normalize();
-
         }
+
         animator.SetFloat("Look X", moveDirection.x);
         animator.SetFloat("Look Y", moveDirection.y);
         animator.SetFloat("Speed", move.magnitude);
-        // Camera.main.transform.position = transform.position + offset; 
+
+        // 确保ArrowIndicator的旋转与角色的移动方向一致
+        if (arrowIndicator != null)
+        {
+            // 计算箭头的旋转角度
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            arrowIndicator.rotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+        }
     }
+
     void FixedUpdate()
     {
-        //使用角色自身的坐标系移动
-        move = move.x*transform.right + move.y*transform.up;
+        // 使用角色自身的坐标系移动
+        move = move.x * transform.right + move.y * transform.up;
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
