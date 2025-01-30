052afa68db6fbfb7a72d3d392e0e145be1823c6c
xsk23 <xsk23@mails.tsinghua.edu.cn>
2025-01-31 01:02:44 +0800

    新增简易大地图和任务栏

--- Degrade_project/Assets/Scripts/RotatingCamera.cs
+++ Degrade_project/Assets/Scripts/RotatingCamera.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotatingCamera : MonoBehaviour
@@ -7,19 +6,25 @@ public class RotatingCamera : MonoBehaviour
     public float rotateTime = 0.2f;
     private Transform player;
     private bool isRotating = false;
-    
-    // Start is called before the first frame update
+
+    // 小地图和大地图摄像头
+    public Transform miniMapCamera;
+    // public Transform BigMapCamera;
+
+    // 保存当前的旋转角度
+    public float currentRotation = 0f;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = player.position;
         Rotate();
     }
+
     void Rotate()
     {
         if (Input.GetKey(KeyCode.Q) && !isRotating)
@@ -31,15 +36,36 @@ public class RotatingCamera : MonoBehaviour
             StartCoroutine(RotateAround(-45, rotateTime));
         }
     }
-    IEnumerator RotateAround(float angle,float time){
-        float number = 60*time;
-        float nextAngle = angle/number;
+
+    IEnumerator RotateAround(float angle, float time)
+    {
+        float steps = 60 * time;
+        float anglePerStep = angle / steps;
         isRotating = true;
-        for(int i=0;i<number;i++){
-            transform.Rotate(new Vector3(0,0,nextAngle));
-            yield return new WaitForFixedUpdate(); 
+
+        for (int i = 0; i < steps; i++)
+        {
+            // 主摄像头旋转
+            transform.Rotate(new Vector3(0, 0, anglePerStep));
+
+            // 更新当前旋转角度
+            currentRotation += anglePerStep;
+            currentRotation %= 360;
+
+            // 同步小地图摄像头旋转
+            if (miniMapCamera != null)
+            {
+                miniMapCamera.Rotate(new Vector3(0, 0, anglePerStep));
+            }
+
+            // // 同步大地图摄像头旋转
+            // if (BigMapCamera != null)
+            // {
+            //     BigMapCamera.Rotate(new Vector3(0, 0, anglePerStep));
+            // }
+
+            yield return new WaitForFixedUpdate();
         }
         isRotating = false;
     }
 }
-
