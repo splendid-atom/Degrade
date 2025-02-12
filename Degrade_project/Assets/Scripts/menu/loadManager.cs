623ff93975a9e500943a8a5742e0a0a59c1cb0b9
splendid-atom <1549444829@qq.com>
2025-02-12 12:05:09 +0800

    hrf_2/12

---
+++ Degrade_project/Assets/Scripts/menu/loadManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Threading;
+
+public class loadManager : MonoBehaviour
+{
+    public GameObject loadScreen;
+    public GameObject loadSetting;
+    public Slider slider;
+    public Text text;
+    private int timer;
+
+    public void loadNextLevel()
+    {
+        timer = 0;
+        StartCoroutine(loadLevel());
+    }
+    public void openSetting()
+    {
+        loadSetting.SetActive(true);
+    }
+
+    public void closeSetting()
+    {
+        loadSetting.SetActive(false);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator loadLevel()
+    {
+        loadScreen.SetActive(true);
+        AsyncOperation operation = SceneManager.LoadSceneAsync("SampleScene");
+        operation.allowSceneActivation = false;
+        /*while (!operation.isDone)
+        {
+            slider.value = operation.progress;
+            text.text = operation.progress * 100 + "%";
+            if (operation.progress >= 0.9f)
+            {
+                slider.value = 1;
+                text.text = "Press any key to continue";
+                if (Input.anyKeyDown)
+                {
+                    operation.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }*/
+        while (timer <= 100)
+        {
+            slider.value = timer / 100f;
+            Debug.Log(slider.value);
+            Debug.Log(timer);
+            text.text = timer + "%";
+            timer += 1;
+            yield return new WaitForSeconds(0.05f);
+
+        }
+        operation.allowSceneActivation = true;
+    }
+}
